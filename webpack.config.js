bcf6307a8482709db665f32a891aa761388ac0b6
Pravin Mohite <Pravin_Mohite@epam.com>
2020-11-29 14:32:55 +0530

    webpack file added

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './media/js/app.js',
+  mode:'development',
+  devServer:{
+    open:true
+  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:"index.html",
+    })
+  ]
+};
